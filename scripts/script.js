fba24e1b5dd60db55fde91ca4b311ae2385f8a86
annaindistress <annartmva@gmail.com>
2022-08-10 17:01:55 +0300

    Update form validation

--- scripts/script.js
+++ scripts/script.js
@@ -1,8 +1,23 @@
-const formInput = document.querySelector('.form__input');
-const formMessage = document.querySelector('.form__message');
+const form = document.querySelector('.form');
+const formInput = form.querySelector('.form__input');
+const formMessage = form.querySelector('.form__message');
+
+const validateForm = () => {
+  if (!formInput.validity.valid) {
+    formInput.classList.add('form__input--invalid');
+    formMessage.classList.add('form__message--visible');
+  } else {
+    formInput.classList.remove('form__input--invalid');
+    formMessage.classList.remove('form__message--visible');
+  }
+};
 
 formInput.addEventListener('invalid', (evt) => {
   evt.preventDefault();
-  formInput.classList.add('form__input--invalid');
-  formMessage.classList.add('form__message--visible');
+  validateForm();
+});
+
+form.addEventListener('submit', (evt) => {
+  evt.preventDefault();
+  validateForm();
 });
